5b852eee0d0297e2191ae515f44772cc792dfc15
asalama97 <asalama@wustl.edu>
2017-05-02 01:22:48 -0500

    Update Button_Test.ino

--- Button_Test.ino
+++ Button_Test.ino
@@ -15,23 +15,23 @@ void setup() {
 void loop() {
   // read the state of the button value:
   buttonState = digitalRead(buttonPin);
-  
+
   // check if the pushbutton is pressed.
   // if it is, the buttonState is HIGH:
   if (buttonState == HIGH) {
-    if(state==0){                     // If the system was off when the button was pressed, turn it on
-         state = 1;
+    if (state == 0) {                 // If the system was off when the button was pressed, turn it on
+      state = 1;
     }
     else if (state == 1) {          // Otherwise, turn it off
       state = 0;
-      
+
     }
   }
-  
-Serial.print("Button State: ");   // Print values onto Serial monitor
-Serial.println(state);
 
-    //delay(500);                 // Delay purely for printing purposes
-                                  // Comment it out to see the print outs more clearly
+  Serial.print("Button State: ");   // Print values onto Serial monitor
+  Serial.println(state);
+
+  //delay(500);                 // Delay purely for printing purposes
+                                // Comment it out to see the print outs more clearly
 
 }
